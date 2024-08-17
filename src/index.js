9ed8b9bf309d4a05de1b8a6742928608bb0598f9
Максим Жук <maxim.thudercat@gmail.com>
2024-08-18 00:21:33 +0300

    Completed Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const bracketsMap = {};
+  const openBrackets = [];
+
+  bracketsConfig.forEach(([open, close]) => {
+    bracketsMap[close] = open;
+    openBrackets.push(open);
+  });
+
+  for (let char of str) {
+    if (openBrackets.includes(char)) {
+      if (stack.length > 0 && stack[stack.length - 1] === char && bracketsMap[char] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
